a85b56ab2833c5dc2bae8746963a59d1f0d0d811
Jovan Toskic <jovan@Jovans-MacBook-Pro.local>
2021-07-27 13:23:48 +0200

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Routes from './routes';
+
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes>
+      <App />
+    </Routes>
   </React.StrictMode>,
   document.getElementById('root')
 );
